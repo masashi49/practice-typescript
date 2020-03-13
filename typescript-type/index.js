f707671f35e8261d599aa48791476cf202668547
yamada <yamada@reas.jp>
2020-03-13 14:20:12 +0900

    型推論

---
+++ typescript-type/index.js
@@ -0,0 +1,11 @@
+var hasValue = true;
+var count = 0;
+var float = 3.123;
+var negative = 3.123;
+var single = 'hello';
+var double = "helloo";
+var back = "`hello`";
+var hello = 2;
+var person = {
+    name: 'jack'
+};
---
+++ typescript-type/index.ts
@@ -0,0 +1,15 @@
+let hasValue:boolean = true;
+let count :number = 0;
+let float:number = 3.123;
+let negative:number = 3.123;
+let single:string = 'hello';
+let double:string = "helloo"
+let back:string = "`hello`"
+
+let hello = 2;
+
+const person:{
+  name:string
+} = {
+  name: 'jack'
+}
