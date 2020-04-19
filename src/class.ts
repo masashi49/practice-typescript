78e451c996b50bbefdf50b61a8636f0507a05458
山田将史 <masashisama94@gmail.com>
2020-04-20 08:25:35 +0900

    Person

---
+++ src/class.ts
@@ -0,0 +1,10 @@
+class Person {
+    name: string; //nameフィールド
+    constructor(initName: string) { //値を受け取れる
+        this.name = initName;
+    }
+}
+
+const jonn = new Person('ジョン');
+
+console.log(jonn)
