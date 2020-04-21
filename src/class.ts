371aa727bc8559c6e551562b5b40c4e028a26425
山田将史 <masashisama94@gmail.com>
2020-04-22 01:16:31 +0900

    thisはコンテキストによって変化すると知っておこう

--- src/class.ts
+++ src/class.ts
@@ -5,6 +5,16 @@ class Person {
     constructor(initName: string) { //値を受け取れる
         this.name = initName;
     }
-}
 
+    greeting() {
+        console.log(`${this.name}`)
+    }
+}
 const jonn = new Person('ジョン');
+jonn.greeting();
+
+const onotherGreeting = {
+    name: 'wao',
+    test: jonn.greeting,
+}
+onotherGreeting.test();
